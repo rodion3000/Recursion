e75741b08cb70e7192b9536e86441f9b7b5c61ba
rodion3000 <rodion.papizh@mail.ru>
2022-04-26 12:03:42 +0500

    Добавьте файлы проекта.

---
+++ Recursion/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace FirstApp
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите что-то");
+            var str = Console.ReadLine();
+            Console.WriteLine("Введите глубину эха");
+            var deep = int.Parse(Console.ReadLine());
+            Echo(str, deep);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+        static void Echo(string saidworld, int deep)
+        {
+            var modif = saidworld;
+            if (modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2);
+                Console.WriteLine("..." + modif);
+            }
+            if (deep > 1)
+            {
+                Echo(modif, deep - 1);
+            }
+        }
+
+    }
+
+
+}
